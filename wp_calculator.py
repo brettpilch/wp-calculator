4235607ecd58208a107ea732264b61616dfcf745
Brett Pilcher <brettpilch@yahoo.com>
2014-12-11 21:05:19 -0600

    horizontal radio buttons, clean up, display all results

--- wp_calculator.py
+++ wp_calculator.py
@@ -1,12 +1,12 @@
 from parser import *
 from Tkinter import *
 
-full = 'http://wp.advancedfootballanalytics.com/winprobcalc22.php?scorediff=0&minleft=5&sec=00&ydline=22&fldside=own&qtr=4&down=1&togo=10&preGameWP=0.50&ko1stHalf=3'
+# Advanced Football Analysis API stem:
 stem = 'http://wp.advancedfootballanalytics.com/winprobcalc22.php?'
 
 # Default Values:
 scorediff_default = '0'
-qtr_default = '4'
+qtr_default = '1'
 qtr_values = ['1','2','3','4','1st OT Poss', 'OT down by 3', 'OT sudden death']
 minleft_default = '5'
 secleft_default = '00'
@@ -18,11 +18,17 @@ togo_default = '10'
 preGameWP_default = '0.50'
 ko1stHalf_default = '3'
 
+
 app = Tk()
 app.title("NFL Win Probability Calculator")
 app.geometry("600x800+10+10")
 
 wp = StringVar()
+awp = StringVar()
+ep = StringVar()
+first_prob = StringVar()
+td_prob = StringVar()
+fg_prob = StringVar()
 
 def calculate_wp():
     fields = [
@@ -41,154 +47,139 @@ def calculate_wp():
     endlist = ['='.join(field) for field in fields]
     end = '&'.join(endlist)
     url = stem + end
-    print url
-    #print url == full
+    #print url
 
     page = get_page(url)
     table = get_all_tables(page)[0]
-    wp.set('Win Probability: ' + table[0][1])
+    wp.set(table[0][0] + ' ' + table[0][1])
+    awp.set(table[1][0] + ' ' + table[1][1])
+    ep.set(table[2][0] + ' ' + table[2][1])
+    first_prob.set(table[3][0] + ' ' + table[3][1])
+    td_prob.set(table[4][0] + ' ' + table[4][1])
+    fg_prob.set(table[5][0] + ' ' + table[5][1])
 
-scorediff_label = Label(app, text = 'Score Difference:')
-scorediff_label.pack()
+score_frame = Frame(app)
+score_frame.pack()
+scorediff_label = Label(score_frame, text = 'Score Difference:')
+scorediff_label.pack(side=LEFT)
 
 scorediff = StringVar()
 scorediff.set(scorediff_default)
-scorediff_entry = Entry(app, textvariable = scorediff)
-scorediff_entry.pack()
+scorediff_entry = Entry(score_frame, textvariable = scorediff)
+scorediff_entry.pack(side=LEFT)
 
-qtr_label = Label(app, text = 'Quarter:')
-qtr_label.pack()
-
+qtr_frame1 = Frame(app)
+qtr_frame1.pack()
+qtr_frame2 = Frame(app)
+qtr_frame2.pack()
+qtr_label = Label(qtr_frame1, text = 'Quarter:')
+qtr_label.pack(side=LEFT)
 qtr = StringVar()
-qtr.set(qtr_default)
-qtr_dropdown = OptionMenu(app, qtr, *qtr_values) # need callback function?
-qtr_dropdown.pack()
+qtr_b1 = Radiobutton(qtr_frame1, text="1", variable=qtr, value='1')
+qtr_b1.pack(side=LEFT)
+qtr_b1.select()
+Radiobutton(qtr_frame1, text="2", variable=qtr, value='2').pack(side=LEFT)
+Radiobutton(qtr_frame1, text="3", variable=qtr, value='3').pack(side=LEFT)
+Radiobutton(qtr_frame1, text="4", variable=qtr, value='4').pack(side=LEFT)
+Radiobutton(qtr_frame2, text="1st OT poss", variable=qtr, value='5').pack(side=LEFT)
+Radiobutton(qtr_frame2, text="OT down 3 pts", variable=qtr, value='6').pack(side=LEFT)
+Radiobutton(qtr_frame2, text="OT sudden death", variable=qtr, value='7').pack(side=LEFT)
 
-minleft_label = Label(app, text = 'Minutes:')
-minleft_label.pack()
+timeleft_frame = Frame(app)
+timeleft_frame.pack()
+timeleft_label = Label(timeleft_frame, text = 'Time Remaining:')
+timeleft_label.pack(side=LEFT)
 
 minleft = StringVar()
 minleft.set(minleft_default)
-minleft_entry = Entry(app, textvariable = minleft)
-minleft_entry.pack()
+minleft_entry = Entry(timeleft_frame, textvariable = minleft)
+minleft_entry.pack(side=LEFT)
 
-secleft_label = Label(app, text = 'Seconds:')
-secleft_label.pack()
+secleft_label = Label(timeleft_frame, text = ':')
+secleft_label.pack(side=LEFT)
 
 secleft = StringVar()
 secleft.set(secleft_default)
-secleft_entry = Entry(app, textvariable = secleft)
-secleft_entry.pack()
+secleft_entry = Entry(timeleft_frame, textvariable = secleft)
+secleft_entry.pack(side=LEFT)
 
-ydline_label = Label(app, text = 'Yard Line:')
-ydline_label.pack()
+ydline_frame = Frame(app)
+ydline_frame.pack()
+ydline_label = Label(ydline_frame, text = 'Yard Line:')
+ydline_label.pack(side=LEFT)
 
 ydline = StringVar()
 ydline.set(ydline_default)
-ydline_entry = Entry(app, textvariable = ydline)
-ydline_entry.pack()
+ydline_entry = Entry(ydline_frame, textvariable = ydline)
+ydline_entry.pack(side=LEFT)
 
-fldside_label = Label(app, text = 'Field Side:')
-fldside_label.pack()
+##fldside_label = Label(app, text = 'Field Side:')
+##fldside_label.pack()
 
 fldside = StringVar()
-Radiobutton(app, text="Own", variable=fldside, value='own').pack()
-Radiobutton(app, text="Opponent's", variable=fldside, value='opp').pack()
+fldside_b1 = Radiobutton(ydline_frame, text="Own", variable=fldside, value='own')
+fldside_b1.pack(side=LEFT)
+fldside_b1.select()
+Radiobutton(ydline_frame, text="Opponent's", variable=fldside, value='opp').pack(side=LEFT)
 
-down_label = Label(app, text = 'Down:')
-down_label.pack()
+down_frame = Frame(app)
+down_frame.pack()
+down_label = Label(down_frame, text = 'Down:')
+down_label.pack(side=LEFT)
 
 down = StringVar()
-Radiobutton(app, text="1st", variable=down, value='1').pack()
-Radiobutton(app, text="2nd", variable=down, value='2').pack()
-Radiobutton(app, text="3rd", variable=down, value='3').pack()
-Radiobutton(app, text="4th", variable=down, value='4').pack()
+down_b1 = Radiobutton(down_frame, text="1st", variable=down, value='1')
+down_b1.pack(side=LEFT)
+down_b1.select()
+Radiobutton(down_frame, text="2nd", variable=down, value='2').pack(side=LEFT)
+Radiobutton(down_frame, text="3rd", variable=down, value='3').pack(side=LEFT)
+Radiobutton(down_frame, text="4th", variable=down, value='4').pack(side=LEFT)
 
-togo_label = Label(app, text = 'To go distance:')
-togo_label.pack()
+togo_frame = Frame(app)
+togo_frame.pack()
+togo_label = Label(togo_frame, text = 'To go distance:')
+togo_label.pack(side=LEFT)
 
 togo = StringVar()
 togo.set(togo_default)
-togo_entry = Entry(app, textvariable = togo)
-togo_entry.pack()
+togo_entry = Entry(togo_frame, textvariable = togo)
+togo_entry.pack(side=LEFT)
 
-preGameWP_label = Label(app, text = 'Pre-game win probability:')
-preGameWP_label.pack()
+pregame_frame = Frame(app)
+pregame_frame.pack()
+preGameWP_label = Label(pregame_frame, text = 'Pre-game win probability:')
+preGameWP_label.pack(side=LEFT)
 
 preGameWP = StringVar()
 preGameWP.set(preGameWP_default)
-preGameWP_entry = Entry(app, textvariable = preGameWP)
-preGameWP_entry.pack()
+preGameWP_entry = Entry(pregame_frame, textvariable = preGameWP)
+preGameWP_entry.pack(side=LEFT)
 
-ko1stHalf_label = Label(app, text = 'Offense kicked off in first half?')
-ko1stHalf_label.pack()
-
+ko_frame = Frame(app)
+ko_frame.pack()
+ko1stHalf_label = Label(ko_frame, text = 'Offense kicked off in first half?')
+ko1stHalf_label.pack(side=LEFT)
 ko1stHalf = StringVar()
-Radiobutton(app, text="yes", variable=ko1stHalf, value='1').pack()
-Radiobutton(app, text="no", variable=ko1stHalf, value='2').pack()
-Radiobutton(app, text="don't know", variable=ko1stHalf, value='3').pack()
+Radiobutton(ko_frame, text="yes", variable=ko1stHalf, value='1').pack(side=LEFT)
+Radiobutton(ko_frame, text="no", variable=ko1stHalf, value='2').pack(side=LEFT)
+ko1stHalf_b1 = Radiobutton(ko_frame, text="don't know", variable=ko1stHalf, value='3')
+ko1stHalf_b1.pack(side=LEFT)
+ko1stHalf_b1.select()
 
 submit_button = Button(app, text = 'Calculate Win Probability', width = 40, command = calculate_wp)
 submit_button.pack()
 
-wp.set('Win Probability: 0.50')
 wp_label = Label(app, textvariable = wp)
 wp_label.pack()
+awp_label = Label(app, textvariable = awp)
+awp_label.pack()
+ep_label = Label(app, textvariable = ep)
+ep_label.pack()
+first_label = Label(app, textvariable = first_prob)
+first_label.pack()
+td_label = Label(app, textvariable = td_prob)
+td_label.pack()
+fg_label = Label(app, textvariable = fg_prob)
+fg_label.pack()
 
 app.mainloop()
-
-##
-##while True:
-##    scorediff1 = raw_input('Score Diff int(-inf, inf): ')
-##    if scorediff1: scorediff = scorediff1
-##    
-##    qtr1 = raw_input('Quarter int[1,7] (5: 1st OT possession, 6: OT -3 pts, 7: sudden death) ')
-##    if qtr1: qtr = qtr1
-##    
-##    timeleft1 = raw_input('Time Left (mm:ss <= 15:00) ')
-##    if timeleft1: timeleft = timeleft1
-##    
-##    ydline1 = raw_input('Yard Line int[1,50]: ')
-##    if ydline1: ydline = ydline1
-##    
-##    fldside1 = raw_input('Field Side [own, opp]: ')
-##    if fldside1: fldside = fldside1
-##    
-##    down1 = raw_input('Down int[1,4]: ')
-##    if down1: down = down1
-##    
-##    togo1 = raw_input('To go distance int[1,90]: ')
-##    if togo1: togo = togo1
-##    
-##    preGameWP1 = raw_input('Pre-game WP float[0.00,1.00): ')
-##    if preGameWP1: preGameWP = preGameWP1
-##    
-##    ko1stHalf1 = raw_input('Offense kicked off in 1st half? (1: yes, 2: no, 3: don\'t know) ')
-##    if ko1stHalf1: ko1stHalf = ko1stHalf1
-##    
-##    timelist = timeleft.split(':')
-##
-##    fields = [
-##        ['scorediff', scorediff],
-##        ['minleft', timelist[0]],
-##        ['sec', timelist[1]],
-##        ['ydline', ydline],
-##        ['fldside', fldside],
-##        ['qtr', qtr],
-##        ['down', down],
-##        ['togo', togo],
-##        ['preGameWP', preGameWP],
-##        ['ko1stHalf', ko1stHalf]
-##        ]
-##
-##    endlist = ['='.join(field) for field in fields]
-##    end = '&'.join(endlist)
-##    url = stem + end
-##    #print url
-##    #print url == full
-##
-##    page = get_page(url)
-##    tables = get_all_tables(page)
-##    for table in tables:
-##        for row in table:
-##            print row
